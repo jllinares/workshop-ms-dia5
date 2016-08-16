51f4603b5e7c25120956bdc151603daccca17306
javier.jose.llinares@gmail.com <Javier2662>
2016-08-15 22:24:26 -0500

    Cmmit

--- src/main/java/org/jllinares/Lab05Application.java
+++ src/main/java/org/jllinares/Lab05Application.java
@@ -1,12 +1,17 @@
 package org.jllinares;
 
+import org.jllinares.services.SaludaService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Lab05Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab05Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Lab05Application.class, args);
+		SaludaService service = context.getBean(SaludaService.class);
+		String saludo = service.saluda("Alumnos");
+		System.out.println(saludo);
 	}
 }
