7788de892ee6aa5316da3bd0b87f24769722fe33
javier.jose.llinares@gmail.com <Javier2662>
2016-08-15 21:48:01 -0500

    Inicio de exportacion

---
+++ src/main/java/org/jllinares/Lab05Application.java
@@ -0,0 +1,12 @@
+package org.jllinares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab05Application.class, args);
+	}
+}
